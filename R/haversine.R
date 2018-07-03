00f4ef9c4603414fc942d82d0afae96a532f5640
Michael Malick <malickmj@gmail.com>
2018-07-03 06:58:39 -0700

    Clean whitespace

--- R/haversine.R
+++ R/haversine.R
@@ -1,11 +1,11 @@
 #' @title Compute great circle distance between two points
-#' 
+#'
 #' @description
-#'     This function computes the great circle distance between 
+#'     This function computes the great circle distance between
 #'     two points given their latitiude and longitude (in decimal
 #'     degrees) using the haversine formula. The output is the distance
 #'     between the two points in km.
-#' 
+#'
 #' @param lat1
 #'     latitude of first point
 #' @param lon1
@@ -14,18 +14,18 @@
 #'     latitude of second point
 #' @param lon2
 #'     longitude of second point
-#' 
+#'
 #' @return
 #'     The function outputs a scalar giving the great circle distance in
 #'     kilometers.
-#' 
+#'
 #' @author Michael Malick
-#' 
+#'
 #' @export
-#' 
+#'
 #' @examples
 #' haversine(48, -147, 49, -154)
-#' 
+#'
 haversine <- function(lat1, lon1, lat2, lon2) {
 
     # Convert degrees to radians
@@ -37,7 +37,7 @@ haversine <- function(lat1, lon1, lat2, lon2) {
     R <- 6371 # Earth mean radius [km]
     delta.lon <- (lon2 - lon1)
     delta.lat <- (lat2 - lat1)
-    a <- sin(delta.lat/2)^2 + cos(lat1) * cos(lat2) * 
+    a <- sin(delta.lat/2)^2 + cos(lat1) * cos(lat2) *
          sin(delta.lon/2)^2
     c <- 2 * asin(min(1,sqrt(a)))
     d = R * c
