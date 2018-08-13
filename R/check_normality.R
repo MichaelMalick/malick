0f6f2dfe967066aa83005b949f4c5915ca41c949
Michael Malick <malickmj@gmail.com>
2018-08-13 07:47:35 -0700

    Add check_normality and check_outliers functions

---
+++ R/check_normality.R
@@ -0,0 +1,117 @@
+#' @title Check the normality assumption of a vector
+#'
+#' @description
+#'  Check the normality assumption of univariate data or model residuals using
+#'  graphical methods.
+#'
+#' @param x
+#'  numeric vector
+#' @param panel
+#'  logical, if \code{TRUE} a multi-panel graphic is produced, otherwise each
+#'  graphic is printed individually.
+#' @param na.rm
+#'  logical, should NA values be removed
+#' @param shapiro.test
+#'  logical, should a Shapiro-Wilks test be run
+#'
+#' @return Prints a series of plots:
+#'  \item{Histogram}{Histogram of data with empirical density overlay (red)
+#'                   and a theoretical normal PDF overlay (grey)}
+#'  \item{QQ plot}{Normal QQ plot}
+#'  \item{CDF}{Empirical (red dots) and theoretical normal (grey line)
+#'             cumulative distributions}
+#'
+#'  If \code{shapiro.test}, then the results of the test are returned
+#'
+#' @author Michael Malick
+#'
+#' @export
+#'
+#' @seealso
+#'  \code{\link[stats]{dnorm}}
+#'  \code{\link[stats]{pnorm}}
+#'  \code{\link[stats]{density}}
+#'  \code{\link[stats]{plot.ecdf}}
+#'  \code{\link[stats]{shapiro.test}}
+#'
+#' @examples
+#'  check_normality(rnorm(100))
+#'  check_normality(rnorm(100), panel = FALSE)
+#'  check_normality(rlnorm(100))
+#'
+check_normality <- function(x, panel = TRUE,
+                            na.rm = FALSE,
+                            shapiro.test = FALSE) {
+
+    if(!is.numeric(x)) stop("x is not numeric")
+
+    if(panel) {
+        .par <- graphics::par(no.readonly = TRUE)
+        graphics::par(mfrow = c(2, 2), mar = c(4, 4, 5, 2))
+    }
+
+    if(na.rm) x <- x[!is.na(x)]
+
+    ## Theoretical density
+    x.extend <- grDevices::extendrange(x, f = 0.15)
+    x.seq    <- seq(x.extend[1], x.extend[2], length = 100)
+    x.dnorm  <- stats::dnorm(x.seq, mean(x), stats::sd(x))
+
+    ## Emprirical density
+    x.den <- stats::density(x, adjust = 1.5)
+
+    ## Save histogram values
+    x.hist <- graphics::hist(x, plot = FALSE)
+
+    ## Compute ylim values
+    ylim <- c(0, max(x.hist$density, x.den$y, x.dnorm))
+
+    ## Histogram
+    graphics::hist(x, main = "",
+                   freq = FALSE,
+                   ylim = ylim,
+                   col = "grey70",
+                   border = "white",
+                   bty = "n",
+                   axes = FALSE,
+                   panel.first = graphics::grid(lty = 1, col = "grey90"))
+    graphics::axis(side = 1, lwd = 0, lwd.tick = 1, col = "grey50")
+    graphics::axis(side = 2, lwd = 0, lwd.tick = 1, las = 1, col = "grey50")
+    graphics::box(col = "grey50")
+    graphics::lines(x.seq, x.dnorm, col = "grey50", lwd = 2)
+    graphics::lines(x.den, col = "red3", lwd = 2)
+    graphics::rug(x, col = "grey50")
+    graphics::mtext("Empirical and Normal PDF", font = 2, line = 1)
+    # graphics::title(sub = "Empirical = red curve, Theoretical = grey curve")
+    # graphics::legend("topright", legend = c("Empirical", "Theoretical"),
+    #                  lwd = 2, col = c("red3", "grey50"), bty = "n")
+
+
+    ## QQ Plot
+    stats::qqnorm(x, col = "red3", pch = 19, main = "",
+                  cex = 0.7,
+                  axes = FALSE,
+                  panel.first = {graphics::grid(lty = 1, col = "grey90")
+                                 stats::qqline(x, col = "grey50", lwd = 1.5)})
+    graphics::axis(side = 1, lwd = 0, lwd.tick = 1, col = "grey50")
+    graphics::axis(side = 2, lwd = 0, lwd.tick = 1, las = 1, col = "grey50")
+    graphics::box(col = "grey50")
+    graphics::mtext("Normal Q-Q Plot", font = 2, line = 1)
+
+
+    ## CDF Plot
+    y <- seq(x.extend[1], x.extend[2], length = 100)
+    stats::plot.ecdf(x, main = "", col = "red3", cex = 0.7,
+                     axes = FALSE,
+                     panel.first = {graphics::grid(lty = 1, col = "grey90")
+                     graphics::lines(y, stats::pnorm(y, mean(x), stats::sd(x)),
+                                     col = "grey50", lwd = 1.5)})
+    graphics::axis(side = 1, lwd = 0, lwd.tick = 1, col = "grey50")
+    graphics::axis(side = 2, lwd = 0, lwd.tick = 1, las = 1, col = "grey50")
+    graphics::box(col = "grey50")
+    graphics::mtext("Empirical and Normal CDF", font = 2, line = 1)
+
+    if(panel) graphics::par(.par)
+
+    if(shapiro.test) stats::shapiro.test(x)
+}
---
+++ R/check_outliers.R
@@ -0,0 +1,101 @@
+#' @title Find potential outliers in a numeric vector
+#'
+#' @description
+#'  Identifies potential outliers in a numeric vector based on threshold values
+#'  computed from input data.
+#'
+#' @param x
+#'  numeric vector
+#' @param method
+#'  method for finding potential outliers
+#' @param range
+#'  multiplier of IQR for outlier threshold
+#' @param na.rm
+#'  logical, should NA values be removed
+#' @param plot
+#'  logical, should a plot of results be produced
+#'
+#' @return
+#'  Returns a list with elements:
+#'    \item{outlier}{logical vector the same length as \code{x} where
+#'          \code{TRUE} indicates point is outside threshold values and
+#'          \code{FALSE} otherwise}
+#'    \item{index}{indices of x that are outside threshold}
+#'    \item{threshold}{vector of length two giving lower and upper
+#'          thresholds}
+#'    \item{n.outlier}{number of points outside threshold values}
+#'
+#'  If \code{plot = TRUE}, then a plot of \code{x} is produced with potential
+#'  outliers highlighted in red. Dashed horizontal lines indicate outlier
+#'  thresholds.
+#'
+#' @details
+#'  Currently only \code{iqr} method is implemented.
+#'
+#'  Method details:
+#'      \itemize{
+#'        \item{\code{iqr} identifies an "outlier threshold" based on the
+#'                   interquartile range (IQR) of the data. The upper/lower
+#'                   threshold values are calculated as +/- IQR * range.}
+#'      }
+
+#' @author Michael Malick
+#'
+#' @export
+#'
+#' @seealso
+#'  \code{\link[stats]{quantile}}
+#'
+#' @examples
+#'  check_outliers(c(rnorm(100), 10, -10))
+#'  check_outliers(exp(rnorm(100)), range = 5)
+#'  check_outliers(exp(rnorm(100)))
+#'
+check_outliers <- function(x, method = "iqr",
+                           range = 3,
+                           na.rm = FALSE,
+                           plot = TRUE) {
+
+    if(!is.numeric(x)) stop("x is not numeric")
+
+    if(na.rm) x <- x[!is.na(x)]
+
+    if(method == "iqr") {
+        qrt <- stats::quantile(x)
+        q1  <- qrt[2]
+        q3  <- qrt[4]
+        iqr <- q3 - q1
+        l.thresh <- q1 - (iqr * range)
+        u.thresh <- q3 + (iqr * range)
+        ind <- x < l.thresh | x > u.thresh
+        out <- list(outlier = ind,
+                    index = which(ind),
+                    threshold = c(lower = as.vector(l.thresh),
+                                  upper = as.vector(u.thresh)),
+                    n.outlier = sum(ind))
+        main <- paste0("Outlier: ", range, " * IQR")
+    }
+
+    if(plot) {
+        ind <- out$outlier
+        graphics::plot(x,
+                       pch  = 19,
+                       cex  = 0.9,
+                       col  = "grey50",
+                       ylab = "x",
+                       xlab = "Index",
+                       axes = FALSE,
+                       main = main,
+                       ylim = range(x, l.thresh, u.thresh),
+                       panel.first = graphics::grid(lty = 1, col = "grey90"))
+        graphics::abline(h = out$threshold[1], lty = 2, col = "grey40")
+        graphics::abline(h = out$threshold[2], lty = 2, col = "grey40")
+        graphics::points(which(ind), x[ind], pch = 19, col = "red3", cex = 1.1)
+        graphics::axis(side = 1, lwd = 0, lwd.tick = 1, col = "grey50")
+        graphics::axis(side = 2, lwd = 0, lwd.tick = 1, las = 1, col = "grey50")
+        graphics::rug(x[!ind], ticksize = 0.03, side = 2, col = "grey50")
+        graphics::rug(x[ind], ticksize = 0.03, side = 2, col = "red3")
+        graphics::box(col = "grey50")
+    }
+    return(out)
+}
