8d716f3ec3c29d2f2ebcf4f9839bd1ebd30cde64
Michael Malick <malickmj@gmail.com>
2018-09-01 06:32:48 -0700

    Change default radius in `haversine` to meters
    In the `haversine` function the default radius of the earth was changed from
    kilometers to meters. An `r` input argument was also added to allow the user to
    specify the value of the radius.

--- R/haversine.R
+++ R/haversine.R
@@ -3,7 +3,8 @@
 #' @description
 #'  Computes the great circle distance between two points given their latitude
 #'  and longitude (in decimal degrees) using the haversine formula. The output
-#'  is the distance between the two points in kilometers
+#'  is the distance between the two points in the same units as the input `r`
+#'  (default is meters)
 #'
 #' @param lon1
 #'  longitude of first point
@@ -13,9 +14,12 @@
 #'  longitude of second point
 #' @param lat2
 #'  latitude of second point
+#' @param r
+#'  radius of earth (default is in meters)
 #'
 #' @return
-#'  Outputs a scalar giving the great circle distance in kilometers.
+#'  Outputs a scalar giving the great circle distance in the same units as the
+#'  input radius.
 #'
 #' @author Michael Malick
 #'
@@ -24,7 +28,7 @@
 #' @examples
 #'  haversine(-147, 48, -154, 49)
 #'
-haversine <- function(lon1, lat1, lon2, lat2) {
+haversine <- function(lon1, lat1, lon2, lat2, r = 6378137) {
 
     if(!is.numeric(c(lon1, lat1, lon2, lat2)))
         stop("Inputs are not numeric")
@@ -35,13 +39,12 @@ haversine <- function(lon1, lat1, lon2, lat2) {
     lon2 <- lon2 * pi / 180
     lat2 <- lat2 * pi / 180
 
-    R <- 6371 # Earth mean radius [km]
     delta.lon <- (lon2 - lon1)
     delta.lat <- (lat2 - lat1)
     a <- sin(delta.lat/2)^2 + cos(lat1) * cos(lat2) *
          sin(delta.lon/2)^2
     c <- 2 * asin(min(1,sqrt(a)))
-    d = R * c
+    d <- r * c
 
-    return(d) # Distance in km
+    return(d) # Distance
 }
